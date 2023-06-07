361e3eb0ef1e409e85eb658a2013be29681973b7
Georg Gasser <104206714+DubG1@users.noreply.github.com>
2023-06-07 11:53:23 +0200

    Add files via upload

---
+++ sign_nn.py
@@ -0,0 +1,79 @@
+# Import dependencies
+import torch 
+from PIL import Image
+from torch import nn, save, load
+from torch.optim import Adam
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+from torchvision.datasets import DatasetFolder
+import csv
+import pandas as pd
+
+# Set the data path
+train = pd.read_csv("labels.csv")
+# Set the data path
+train_folder = "C:\\Users\\georg\\OneDrive\\Dokumente\\Studium_Windows\\ml\\data"
+
+
+# Read the labels from the CSV file
+labels = {}
+with open(train_folder, 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        label, image_name = row
+        labels[image_name] = int(label)
+
+# Define the dataset
+dataset = DatasetFolder(root=train_folder, loader=lambda x: Image.open(x), extensions='.jpg', transform=ToTensor(), target_transform=lambda x: labels[x])
+
+
+# Image Classifier Neural Network
+class ImageClassifier(nn.Module): 
+    def __init__(self):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Conv2d(1, 32, (3,3)), 
+            nn.ReLU(),
+            nn.Conv2d(32, 64, (3,3)), 
+            nn.ReLU(),
+            nn.Conv2d(64, 64, (3,3)), 
+            nn.ReLU(),
+            nn.Flatten(), 
+            nn.Linear(64 * 122 * 122, 36)  
+        )
+
+    def forward(self, x): 
+        return self.model(x)
+
+# Instance of the neural network, loss, optimizer 
+clf = ImageClassifier().to('cpu')
+opt = Adam(clf.parameters(), lr=1e-3)
+loss_fn = nn.CrossEntropyLoss() 
+
+# Training flow 
+if __name__ == "__main__": 
+    for epoch in range(10): # train for 10 epochs
+        for batch in dataset: 
+            X,y = batch 
+            X, y = X.to('cpu'), y.to('cpu') 
+            yhat = clf(X) 
+            loss = loss_fn(yhat, y) 
+
+            # Apply backprop 
+            opt.zero_grad()
+            loss.backward() 
+            opt.step() 
+
+        print(f"Epoch:{epoch} loss is {loss.item()}")
+    
+    with open('model_state.pt', 'wb') as f: 
+        save(clf.state_dict(), f) 
+
+    # with open('model_state.pt', 'rb') as f: 
+    #     clf.load_state_dict(load(f))  
+
+    # img = Image.open('img_3.jpg') 
+    # img_tensor = ToTensor()(img).unsqueeze(0).to('cuda')
+
+    # print(torch.argmax(clf(img_tensor)))
